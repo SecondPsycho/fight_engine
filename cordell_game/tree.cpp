61cbd7b42dc6fe3fbdd251f04aae59ff1fb953e6
cking <cordell_king@taylor.edu>
2021-04-28 11:58:33 -0400

    Created Players

--- cordell_game/tree.cpp
+++ cordell_game/tree.cpp
@@ -3,11 +3,12 @@
 
 class NewGame {
   public:
-    NewGame(KinematicBody2D player, int n) {
+    NewGame(Player* player1, Player* player2, int n) {
       this->statics_count = 0;
       this->statics_max = n;
       this->statics = new KinematicBody2D[this->statics_max];
-      this->player = player;
+      this->P1 = player1;
+      this->P2 = player2;
     }
     void addStatic(KinematicBody2D newstatic) {
       if (this->statics_count < this->statics_max) {
@@ -28,17 +29,30 @@ class NewGame {
       return this->statics_count;
     }
   private:
-    KinematicBody2D player;
+    Player* P1;
+    Player* P2;
     KinematicBody2D* statics;
     int statics_count;
     int statics_max;
 };
 
+class Player {
+  public:
+    Player() {
 
+    }
+    //Attributes
+    bool on_ground = false;
+    //Body
+    KinematicBody2D self;
+    animation_data idle;
+    animation_data walk;
+    animation_data leap;
+};
 
 
 int main(int argc, char* argv[]){
-    create_window("Awesome Game", 800, 800);
+    create_window("Up the Tree", 800, 800);
     window.setKeyRepeatEnabled(false);
     
     //Create an Animation
@@ -46,20 +60,34 @@ int main(int argc, char* argv[]){
     animation_data walk;
     animation_data leap;
 
-    //Create Sprite Data
-    idle.addAnimationData(make_sprite("./images/wolf_idle.png"));
-    walk.addAnimationData(make_sprite("./images/wolf_walk/wolf_walk1.png"));
-    walk.addAnimationData(make_sprite("./images/wolf_walk/wolf_walk2.png"));
-    leap.addAnimationData(make_sprite("./images/wolf_leap.png"));
+    Player* P1 = new Player;
+    P1->self = KinematicBody2D(50,100,64,64);
+    P1->self.initHitbox();
 
+    Player* P2 = new Player;
+    P2->self = KinematicBody2D(450,100,64,64);
+    P2->self.initHitbox();
+
+    /*
     KinematicBody2D wolf(50,100,64,64);
-    wolf.setSprite(idle.getCurrentFrame());
+    //wolf.setSprite(idle.getCurrentFrame());
     wolf.initHitbox();
     wolf.getHitbox()->initRectangle(); //Testing
-    //wolf.adjustHitbox(2,4,52,50);
     wolf.a.y = 1;
+    //*/
 
-    NewGame Game(wolf, 6);
+    //Create Sprite Data
+    P1->idle.addAnimationData(make_sprite("./images/wolf_idle.png"));
+    P1->walk.addAnimationData(make_sprite("./images/wolf_walk/wolf_walk1.png"));
+    P1->walk.addAnimationData(make_sprite("./images/wolf_walk/wolf_walk2.png"));
+    P1->leap.addAnimationData(make_sprite("./images/wolf_leap.png"));
+
+    P2->idle.addAnimationData(make_sprite("./images/wolf_idle.png"));
+    P2->walk.addAnimationData(make_sprite("./images/wolf_walk/wolf_walk1.png"));
+    P2->walk.addAnimationData(make_sprite("./images/wolf_walk/wolf_walk2.png"));
+    P2->leap.addAnimationData(make_sprite("./images/wolf_leap.png"));
+
+    NewGame Game(P1, P2, 6);
     Game.addStatic(KinematicBody2D(0,600,800,200));
     Game.getStatic(0)->initRectangle();
     Game.getStatic(0)->initHitbox();
@@ -78,36 +106,46 @@ int main(int argc, char* argv[]){
     Game.getStatic(3)->initHitbox();
     Game.getStatic(3)->v.y = 5;
     
-    //list_dir("./images");
-    //cout << "walk.getSize() = " << walk.getSize() << endl;
 
-    //* Test to the Music
-    Song my_music("sounds/The_Last_Encounter_Original_Version (online-audio-converter.com).wav");
-    my_music.setPitch(1.2f);
-    my_music.setVolume(10.0f);
+    Song my_music("sounds/Restoring the Light, Facing the Dark.wav");
+    //my_music.setPitch(1.2f);
+    //my_music.setVolume(10.0f);
     my_music.play();
     SFX pew_sound("sounds/pew.wav");
     pew_sound.setVolume(100.0f);
-    //*/
         
     Event event;
-    int keys[5] = {0,0,0,0,0};
+    bool keys[10] = {false,false,false,false,false,false,false,false,false,false};
     int *collide;
-    bool wolf_on_ground = false;
+    //bool wolf_on_ground = false;
     Vector2D f(1,0);
+    int worldshifty = 0;
+    int frame, seconds = 0;
 
-    // Framerate Control -Cordell King
     int t = 0;
+    int falldelay = 5;
     Framerate ticker(30);
     while (window.isOpen()) {
       ticker.next_frame();
+      frame += 1;
+      if (frame >= 30) {
+        frame = 0;
+        seconds += 1;
+        cout << seconds << endl;
+        if (seconds >= 82) {
+          falldelay = 1000000000;
+        }
+        if (seconds >= 32) {
+          falldelay = 2;
+        }
+      }
       t += 1;
-      if (t >= 5) {
+      if (t >= falldelay) {
         t = 0;
         for (int i = 0; i < Game.getStaticsCount(); i++) {
           Game.getStatic(i)->p.y += 1;
         }
-        cout << "Worked! Maybe." << Game.getStatic(3)->posY() << endl;
+        worldshifty += 1;
       }
       
       while(window.pollEvent(event)){
@@ -124,24 +162,30 @@ int main(int argc, char* argv[]){
                 //wolf.flipH();
                 break;
               case Keyboard::A:
+                /*
                 keys[2] = 1;
                 if (wolf.isFlippedH()) {
                   wolf.flipH();
                 }
+                //*/
                 break;
               case Keyboard::D:
+                /*
                 keys[3] = 1;
                 if (!wolf.isFlippedH()) {
                   wolf.flipH();
                 }
                 break;
+                //*/
               case Keyboard::Space:
+                /*
                 if (keys[4] == 0 && wolf_on_ground){
-                  pew_sound.play();
+                  //pew_sound.play();
                   keys[4] = 1;
                   wolf.v.y = -20;
                 }
                 break;
+                //*/
               default:
                 break;
             }
@@ -149,13 +193,13 @@ int main(int argc, char* argv[]){
           case Event::KeyReleased:
             switch (event.key.code) {
               case Keyboard::A:
-                keys[2] = 0;
+                //keys[2] = 0;
                 break;
               case Keyboard::D:
-                keys[3] = 0;
+                //keys[3] = 0;
                 break;
               case Keyboard::Space:
-                keys[4] = 0;
+                //keys[4] = 0;
                 break;
               default:
                 break;
@@ -171,29 +215,25 @@ int main(int argc, char* argv[]){
       } else if (Game.getStatic(2)->posX() >= 400) {
         Game.getStatic(2)->v.x = -5;
       }
-      if (Game.getStatic(3)->posY() <= 200) {
+      if (Game.getStatic(3)->posY() <= worldshifty+200) {
         Game.getStatic(3)->v.y = 5;
-      } else if (Game.getStatic(3)->posY() >= 500) {
+      } else if (Game.getStatic(3)->posY() >= worldshifty+500) {
         Game.getStatic(3)->v.y = -5;
       }
 
       //Apply Physics
-      wolf.p.x += 10*(keys[3]-keys[2]);
-      wolf.tick();
-      Game.getStatic(0)->tick();
-      Game.getStatic(1)->tick();
-      Game.getStatic(2)->tick();
-      Game.getStatic(3)->tick();
-
-      //Physics Cleanup
-      //wolf.v.x -= 10*(keys[3]-keys[2]);
-      wolf.dampen(f);
-
-      wolf_on_ground = false;
+      //wolf.p.x += 10*(keys[3]-keys[2]);
+      P1->self.tick();
+      P2->self.tick();
+      P1->self.dampen(f);
+      P2->self.dampen(f);
+      
+      P1->on_ground = false;
       for (int i = 0; i < Game.getStaticsCount(); i++) {
-        collide = Game.getStatic(i)->blocks(&wolf);
+        Game.getStatic(i)->tick();
+        collide = Game.getStatic(i)->blocks(&(P1->self));
         if (collide[2] && collide[1] <= -1) {
-          wolf_on_ground = true;
+          P1->on_ground = true;
         };
       };
 
